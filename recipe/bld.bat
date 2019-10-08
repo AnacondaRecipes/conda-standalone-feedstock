fded31db07a27e4593131b8e9fbab1b75859aab3
Forrest Waters <fwaters@anaconda.com>
2019-10-08 14:47:30 -0500

    embed cli-<arch>.exe into standalone for noarch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,9 @@
 COPY conda_src\conda\core\path_actions.py "%SP_DIR%\conda\core\path_actions.py"
 COPY conda_src\conda\utils.py "%SP_DIR%\conda\utils.py"
 
+:: we need these for noarch packages with entry points to work on windows
+COPY conda_src\conda\shell\cli-%ARCH%.exe entry_point_base.exe
+
 COPY menuinst_src\menuinst\__init__.py "%SP_DIR%\menuinst\__init__.py"
 COPY menuinst_src\menuinst\win32.py "%SP_DIR%\menuinst\win32.py"
 
