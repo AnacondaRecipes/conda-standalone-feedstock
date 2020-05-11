267c374156f968cffe4b5654ad92f195b8b46484
distro-bot@anaconda.com <Distro Bot>
2020-05-11 13:58:13 +0200

    Allow debug builds on Unix (set CONDA_BUILD_CONDA_STANDALONE_DEBUG)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,11 @@
+_SPEC=conda.exe.spec
+declare -a _EXTRA_ARGS=()
+if [[ -n ${CONDA_BUILD_CONDA_STANDALONE_DEBUG} ]]; then
+  # _EXTRA_ARGS+=(--debug=imports)
+  _EXTRA_ARGS+=(--debug=all)
+  _SPEC=conda.exe.dbg.spec
+fi
+
 # patched conda files
 cp conda_src/conda/core/path_actions.py $SP_DIR/conda/core/path_actions.py
 cp conda_src/conda/utils.py $SP_DIR/conda/utils.py
@@ -8,7 +16,11 @@ cp conda_src/conda/utils.py $SP_DIR/conda/utils.py
 
 # -F is to create a single file
 # -s strips executables and libraries
-pyinstaller conda.exe.spec
+pyinstaller "${_EXTRA_ARGS[@]}" ${_SPEC}
+
+# https://pyinstaller.readthedocs.io/en/stable/when-things-go-wrong.html#changing-runtime-behavior
+find . -name "rthooks.dat"
+
 mkdir -p $PREFIX/standalone_conda
 mv dist/conda.exe $PREFIX/standalone_conda
 # clean up .pyc files that pyinstaller creates
