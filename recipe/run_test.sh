5a567b0c812d05b124283cf936e4df72ad961b11
Marco Esters <mesters@anaconda.com>
2025-09-12 16:11:22 -0700

    Set PYINSTALLER_CONDARC_DIR to recipe directory instead of .condarc file

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-export PYINSTALLER_CONDARC_DIR="${RECIPE_DIR}/.condarc"
-test -f "${PYINSTALLER_CONDARC_DIR}"
+export PYINSTALLER_CONDARC_DIR="${RECIPE_DIR}"
+test -d "${PYINSTALLER_CONDARC_DIR}"
 pytest -vvv
 if [[ "$(uname)" == "Darwin" ]]; then
     test ! -e "${PREFIX}/bin/codesign"
