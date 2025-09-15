af9ed7c2ab4dd115d51642808efeaa54c746839e
Marco Esters <mesters@anaconda.com>
2025-09-15 13:57:45 -0700

    Re-add pytest run

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -4,5 +4,8 @@ IF NOT EXIST "%PYINSTALLER_CONDARC_DIR%" (
     ECHO "Could not find directory %PYINSTALLER_CONDARC_DIR%"
     EXIT /B 1
 )
+:: Create a .nonadmin file so that the menuinst tests
+:: do not try to run with admin privileges
 echo. > "%PREFIX%\.nonadmin"
+pytest -vvv
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
