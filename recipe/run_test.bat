d1f6c6dd8a349e975e3dfc7289e2205dddc2da40
Jaida Rice <jaidarice@gmail.com>
2025-10-14 13:05:28 -0700

    Skip test_conda_run

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,4 +1,4 @@
-@ECHO ON 
+@ECHO ON
 SET "PYINSTALLER_CONDARC_DIR=%RECIPE_DIR%"
 IF NOT EXIST "%PYINSTALLER_CONDARC_DIR%" (
     ECHO "Could not find directory %PYINSTALLER_CONDARC_DIR%"
@@ -7,5 +7,5 @@ IF NOT EXIST "%PYINSTALLER_CONDARC_DIR%" (
 :: Create a .nonadmin file so that the menuinst tests
 :: do not try to run with admin privileges
 echo. > "%PREFIX%\.nonadmin"
-pytest -vvv
+pytest -vvv -k "not test_conda_run"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
