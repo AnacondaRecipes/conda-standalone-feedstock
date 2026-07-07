42688dc993feea83de2683558c32702d570c8851
Marco Esters <mesters@anaconda.com>
2026-07-07 07:53:56 -0700

    Skip conda config tests due to conda bug

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,7 +4,9 @@ set -ex
 
 export PYINSTALLER_CONDARC_DIR="${RECIPE_DIR}"
 test -d "${PYINSTALLER_CONDARC_DIR}"
-pytest -vvv
+# conda does not show plug-in settings with `conda config --shows-sources`
+# This will be fixed with https://github.com/conda/conda/pull/16246
+pytest -vvv -k 'not test_conda_standalone_config'
 if [[ "$(uname)" == "Darwin" ]]; then
     test ! -e "${PREFIX}/bin/codesign"
 fi
